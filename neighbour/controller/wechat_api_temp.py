659233e001696c199b49a281de8f88c8bb66bd70
cheaboar <826802085@qq.com>
2015-10-05 17:22:23 +0800

    测试

--- neighbour/controller/wechat_api_temp.py
+++ neighbour/controller/wechat_api_temp.py
@@ -1,1 +1,9 @@
+#****coding=utf-8********
 __author__ = 'Chan'
+
+from wechat_api import wechat_front
+from flask import render_template, request
+
+@wechat_front.route('/new_test')
+def new_test():
+    return 'bad bad'
