f1192056e2fcb688f351bc1389038421846b4078
wujunxi <347348453@qq.com>
2015-10-07 10:20:45 +0800

    （1）添加评价中的星星

--- neighbour/v0.1/wechat/groupbuy/js/evaluate.js
+++ neighbour/v0.1/wechat/groupbuy/js/evaluate.js
@@ -6,6 +6,12 @@
 
     $(function(){
 
+        var $spStars = $("#spStars").delegate("span","click",function(){
+            var $this = $(this),
+                k = $this.attr("key");
+            $spStars.attr("class","stars stars-"+k);
+        });
+
         var $spTip = $("#spTip");
 
         var $taContent = $("#taContent").on("keyup",function(){
